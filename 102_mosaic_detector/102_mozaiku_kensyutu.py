9d898b17ba7c8351fa2cbef56d7a59449bdf09e6
simonmcnair <101189766+simonmcnair@users.noreply.github.com>
2024-02-26 13:34:20 +0000

    Update 102_mozaiku_kensyutu.py
    don't need to create the image files to use the images

--- 102_mosaic_detector/102_mozaiku_kensyutu.py
+++ 102_mosaic_detector/102_mozaiku_kensyutu.py
@@ -11,10 +11,26 @@
 
 import cv2
 import numpy as np
+from PIL import Image
 
 import sys
 args = sys.argv
 
+def make_image(masksize):
+    picturesize = 2+masksize+masksize-1+2
+    screen = (picturesize, picturesize)
+
+    img = Image.new('RGB', screen, (0xff,0xff,0xff))
+
+    pix = img.load()
+
+    for i in range(2,picturesize,masksize-1):
+        for j in range(2,picturesize,masksize-1):
+            for k in range(0,picturesize):
+                pix[i, k] = (0,0,0)
+                pix[k, j] = (0,0,0)
+    return img
+    
 if len(args) != 2:
     print("too few argument.")
     sys.exit(1)
@@ -29,8 +45,8 @@ img_gray = cv2.GaussianBlur(img_gray,(3,3),0) #少しぼかす
 cv2.imwrite('output_gray.png', img_gray)
 
 for i in range(11,20+1):
-    pattern_filename = "../101_pattern_maker/pattern"+str(i)+"x"+str(i)+".png"
-    template = cv2.imread(pattern_filename, 0)
+    image_array = np.array(make_image(i))
+    template = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
     w, h = template.shape[::-1]
 
     img_kensyutu_kekka = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
